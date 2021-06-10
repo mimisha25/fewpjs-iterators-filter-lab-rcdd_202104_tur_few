a0a7ef13dc93be9dcd3499e0dd05365d89e0e70e
Aziza  iliasova <miacorporation25@gmail.com>
2021-06-10 23:23:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+drivers = ['Bobby', 'Sammy', 'Sylly', 'Annette', 'Sarah', 'bobby']
+ function findMatching(drivers, string){
+     const names = drivers.filter(name => name.toLowerCase() === string.toLowerCase())
+     return names
+ }
+
+
+ // const fuzzyMatch =(drivers, string) => {
+ //     const names = drivers.filter(name => name.indexOf(string) === string.indexOf(string))
+ //     return names
+ // }
+
+ function fuzzyMatch(drivers, string){
+     return drivers.filter(name => name.indexOf(string) === string.indexOf(string))
+ }
+
+
+ function matchName(drivers, string){
+     return drivers.filter(driver => driver.name === string)
+ } 
